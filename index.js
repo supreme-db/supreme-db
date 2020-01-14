703d11597404910c69a92ece6d1571e6f48dd0a0
supreme-db <59888144+supreme-db@users.noreply.github.com>
2020-01-14 10:17:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const Database = require('better-sqlite3');
+const fs = require("fs")
 
-let newdb = function(name){
+let new = function(tableName) {
+  
+    // Set Name
+    if (typeof tableName !== 'string') throw new TypeError('Table name has to be a string.');
+    else if (tableName.includes(' ')) throw new TypeError('Table name cannot include spaces.');
+    
+    fs.readFileSync(`${tableName}.json`, function (err) {
+     if(err){
+    fs.writeFile(`${tableName}.json`, function (err) {
+      if (err) throw err;
+      console.log('Table is created successfully.');
+    }); 
+    }else{
+      console.log(`Table: ${tableName} loaded`)
+    }
+})
+  }
+  
+  let add = function(tableName, data){
+    if (typeof tableName !== 'string') throw new TypeError('Table name has to be a string.');
+    else if (tableName.includes(' ')) throw new TypeError('Table name cannot include spaces.');
+    
+    fs.readFileSync(`${tableName}.json`, function (err) {
+      if(err)throw err;
+      if(!err){
+        fs.writeFileSync(`${tableName}.json`, JSON.stringify(data), 'utf8', function(err){
+          if(err)throw err;
+          if(!err)console.log("committed")
+        })
+      }
+    })
+  }
+                    
+  
+  
 
-}
